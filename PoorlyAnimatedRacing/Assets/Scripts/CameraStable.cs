2f44850b7eeabd3f8206e39bac1cbb7343c60cd0
Matt Gillis <gillism@purdue.edu>
2018-12-03 19:03:43 -0500

    Added script to improve camera stability.

---
+++ PoorlyAnimatedRacing/Assets/Scripts/CameraStable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraStable : MonoBehaviour {
+
+    public GameObject theCar;
+    public float carX;
+    public float carY;
+    public float carZ;
+	
+	void Update () 
+    {
+        // get the car's rotation on each axis.
+        carX = theCar.transform.eulerAngles.x;
+        carY = theCar.transform.eulerAngles.y;
+        carZ = theCar.transform.eulerAngles.z;
+
+        // rotate the CameraFocusPoint in the opposite direction of the car's rotation by the same amount
+        // in the x and z axis. Leave the Y because we want the camera to rotate up and down.
+        transform.eulerAngles = new Vector3(carX - carX, carY, carZ - carZ);
+	}
+}
