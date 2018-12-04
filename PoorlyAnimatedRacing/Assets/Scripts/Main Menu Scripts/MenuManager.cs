7e9aba6d632a0ff8f20e36e6a16d732694a6658d
Matt Gillis <gillism@purdue.edu>
2018-12-04 17:30:38 -0500

    Created a start screen and a stage selection screen that connect to each other through buttons, and you can now start the game on the main menu, navigate to the stage selection screen, select track01 and load the scene for the single player version of that race.

---
+++ PoorlyAnimatedRacing/Assets/Scripts/Main Menu Scripts/MenuManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+	
+	
+    public void loadStageSelectionMenu()
+    {
+        SceneManager.LoadScene("StageSelectMenu");
+    }
+
+    public void quitToDesktop ()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+}
---
+++ PoorlyAnimatedRacing/Assets/Scripts/Stage Select Scripts/StageSelectionManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StageSelectionManager : MonoBehaviour {
+
+    public void loadTrack01 ()
+    {
+        SceneManager.LoadScene("Track01_SinglePlayer");
+    }
+}
