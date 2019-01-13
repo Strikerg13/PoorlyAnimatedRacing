9a1c66f8f335c0e37d4a0d0390d9c831d6e07e19
Matt Gillis <gillism@purdue.edu>
2019-01-13 16:42:48 -0500

    Created Track03 spline. Adding navigation triggers to the track.

---
+++ PoorlyAnimatedRacing/Assets/NavigationController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NavigationController : MonoBehaviour {
+
+    // Set these directions in the Scene Editor.
+    public string directions;
+    public GameObject PreviousDirection;
+    public GameObject NextDirection;
+
+    public GameObject NavigationTrigger;
+
+    void OnTriggerEnter()
+    {
+        PreviousDirection.GetComponent<Text>().text = NextDirection.GetComponent<Text>().text;
+
+        NextDirection.GetComponent<Text>().text = directions;
+
+        NavigationTrigger.SetActive(false);
+    }
+}
